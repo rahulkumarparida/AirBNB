3ea47e99c8c236ac5b51361277c43ae65fc7aba9
rahulkumarparida <rahulkumarparida52@gmail.com>
2025-10-03 16:16:07 +0530

    added the profile pic feature for user.

--- backend/airbnbapi/users/serializers.py
+++ backend/airbnbapi/users/serializers.py
@@ -6,11 +6,16 @@ from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
+    profile_pic = serializers.SerializerMethodField()
     class Meta:
         model = Users
         fields = ['id', 'username', 'email', 'role', 'date_joined', 'password' , "profile_pic"]
         read_only_fields = ['id', 'date_joined']
+    
+    def get_profile_pic(self, obj):
+        if obj.profile_pic:
+            return obj.profile_pic.url  
+        return None
 
     def create(self, validated_data):
         role = validated_data.get('role','GU')
