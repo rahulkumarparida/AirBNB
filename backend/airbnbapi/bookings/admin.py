c869376986c6ca6f7f86e0b2e8727f36fa01d175
Rahul Kumar Parida <rahulkumarparida52@gmail.com>
2025-10-04 21:04:30 +0530

    Delete backend/airbnbapi directory

--- backend/airbnbapi/bookings/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from bookings.models import Booking  , Payment 
-
-# Register your models here.
-admin.site.register(Booking)
-admin.site.register(Payment)
--- backend/airbnbapi/users/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path , include
-from users import views
-
-urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name="register"),
-    path('login/', views.LoginView.as_view(), name="login"),
-    path('me/',views.SelfView.as_view(), name="self"),
-]
